b5f9336a592192fd87990dce944d09eec0ec0290
Scott Fisher <scott.fisher@ucop.edu>
2019-11-06 12:52:48 -0800

    making it easier to switch back and forth

--- script/counter-uploader/up_it.rb
+++ script/counter-uploader/up_it.rb
@@ -16,6 +16,10 @@ state_hash = JSON.parse(File.read('json-state/statefile.json'))
 hub_api_token = YAML.load_file('json-state/secrets.yaml')['hub_api_token']
 hub_base_url = YAML.load_file('json-state/config.yaml')['hub_base_url']
 
+hub_api_token = ''
+hub_base_url = 'https://api.datacite.org'
+upload_compressed = false
+
 # a special hack for the uploader class
 ENV['TOKEN'] = hub_api_token
 
@@ -27,24 +31,24 @@ filenames.each do |fn|
   month_key = fn.match(/\d{4}-\d{2}/).to_s
   month_info = state_hash[month_key]
 
-  # all this commented out stuff is for direct upload, which keeps failing for large items.  Trying the compressed upload instead
-  response = HTTParty.put("#{hub_base_url}/reports/#{month_info['id']}",
-    body: File.open(fn, 'r').read,
-    headers: {
-      'Content-Type' => 'application/json',
-      'Accept' => 'application/json',
-      'Authorization' => "Bearer #{hub_api_token}" },
-    timeout: 300)
+  if upload_compressed
+    uploader = Uploader.new(report_id: month_info['id'], file_name: fn)
+    report_id = uploader.process
+    puts "reported id #{report_id}"
+  else
+    response = HTTParty.put("#{hub_base_url}/reports/#{month_info['id']}",
+                            body: File.open(fn, 'r').read,
+                            headers: {
+                                'Content-Type' => 'application/json',
+                                'Accept' => 'application/json',
+                                'Authorization' => "Bearer #{hub_api_token}" },
+                            timeout: 300)
+    # response.headers are useful
+    # response.code should be 200
+    puts response.headers
+    puts response.code
+  end
 
-  # response.headers are useful
-  # response.code should be 200
-  puts response.headers
-  puts response.code
-
-  # uploader = Uploader.new(report_id: month_info['id'], file_name: fn)
-  # report_id = uploader.process
-
-  # puts "reported id #{report_id}"
   puts "Finished #{fn}\r\n\r\n"
 end
 
